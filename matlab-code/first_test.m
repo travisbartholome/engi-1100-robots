6d822aa915aa0b32f2642a61c3815941faaf199a
travisbartholome <travis.bartholome@gmail.com>
2017-06-16 14:46:03 -0500

    Finish basic tests

--- matlab-code/first_test.m
+++ matlab-code/first_test.m
@@ -34,11 +34,45 @@ beep(brick);
 
 %% Other Basics
 
+% Writing to the screen
 clearLCD(brick);
 writeLCD(brick, 'Hello MATLAB!');
 
+% Changing the status light color
 writeStatusLight(brick, 'orange'); % Off, red, green, orange
 
-%% Sensors and Stuff
+% Reading status of the buttons
+buttonStr = 'down'; % Up, down, left, right, center
+buttonStatus = readButton(brick, buttonStr); % 0 or 1 (boolean)
+fprintf('Status of the %s button is %d. \n\n', buttonStr, buttonStatus);
 
+%% Sensors and Stuff - June 16, 2017
 
+% Touch Sensor
+touchPort = 1; % Port the touch sensor is using
+myTouch = touchSensor(brick, touchPort); % Get a handle/object for the sensor
+touchValue = readTouch(myTouch); % Get sensor value (0 or 1)
+fprintf('The value of the touch sensor is %d. \n\n', touchValue);
+
+% Note: this only reads the value a single time, whenever you execute the
+% program. I'll have to do some work to make it run continuously. The
+% `timer` object seems like a good place to start -- similar to that meme I
+% made with Fritz's picture and Starry Night.
+
+% This seems to be working well for what it is, though.
+
+% Ultrasonic Sensor
+USPort = 2; % Port for ultrasonic (US) sensor
+myUS = sonicSensor(brick, USPort);
+USValue = readDistance(myUS); % Distance in meters
+fprintf('The ultrasonic sensor is %f m from the nearest object. \n\n', ...
+    USValue);
+
+% Ultrasonic also seems to be working well. Best, I can get good readings
+% from both the US and touch sensors at the same time.
+
+% I'm going to move to a new file and start trying to continuously update
+% sensors.
+
+%% (Cleanup)
+clear; close all
